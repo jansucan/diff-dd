9ecdd5c0fd0b77682a6bbcfcfeb1d1014b1f538b
Jan Sucan <jan@jansucan.com>
2021-05-30 13:34:38 +0200

    Split the backup code into smaller functions

--- src/backup.c
+++ src/backup.c
@@ -8,8 +8,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-int
-backup(const options_t *const opts, resources_t *const res)
+static int check_files(const options_t *const opts,
+                       const resources_t *const res);
+static int allocate_buffers(const options_t *const opts, size_t out_buffer_size,
+                            resources_t *const res);
+static int read_sector(FILE *const file, char *const buffer,
+                       uint32_t buffer_size, uint32_t sector_size);
+static int write_out_buffer(const char *const buffer, size_t size,
+                            FILE *const file);
+
+static int
+check_files(const options_t *const opts, const resources_t *const res)
 {
     const long in_size = file_size(res->in_file);
 
@@ -36,11 +45,13 @@ backup(const options_t *const opts, resources_t *const res)
         return 1;
     }
 
-    /* Allocate the buffers */
-    /* The output buffer contains also the offsets */
-    const size_t out_buffer_size =
-        ((opts->buffer_size / opts->sector_size) * sizeof(uint64_t)) +
-        opts->buffer_size;
+    return 0;
+}
+
+static int
+allocate_buffers(const options_t *const opts, size_t out_buffer_size,
+                 resources_t *const res)
+{
 
     if ((res->in_buffer = (char *)malloc(opts->buffer_size)) == NULL) {
         print_error("cannot allocate buffer for input file data");
@@ -53,97 +64,109 @@ backup(const options_t *const opts, resources_t *const res)
         return 1;
     }
 
+    return 0;
+}
+
+static int
+read_sector(FILE *const file, char *const buffer, uint32_t buffer_size,
+            uint32_t sector_size)
+{
+    const size_t bytes_read = fread(buffer, 1U, buffer_size, file);
+
+    if (ferror(file)) {
+        print_error("cannot read from input file: %s", strerror(errno));
+        return 1;
+    } else if ((bytes_read % sector_size) != 0) {
+        print_error("data read from input file is not multiple of sector size");
+        return 1;
+    } else if (bytes_read != buffer_size) {
+        print_error("cannot read enough data from input file");
+        return 1;
+    }
+
+    return 0;
+}
+
+static int
+write_out_buffer(const char *const buffer, size_t size, FILE *const file)
+{
+    const size_t bytes_written = fwrite(buffer, 1U, size, file);
+
+    if (bytes_written != size) {
+        print_error("cannot write to output file: %s", strerror(errno));
+        return 1;
+    }
+
+    return 0;
+}
+
+int
+backup(const options_t *const opts, resources_t *const res)
+{
+    if (check_files(opts, res) != 0) {
+        return 1;
+    }
+
+    /* The output buffer contains also the offsets */
+    const size_t out_buffer_size =
+        ((opts->buffer_size / opts->sector_size) * sizeof(uint64_t)) +
+        opts->buffer_size;
+
+    if (allocate_buffers(opts, out_buffer_size, res) != 0) {
+        return 1;
+    }
+
     size_t out_buffer_index = 0;
-    uint64_t offset = 0;
+    uint64_t input_file_offset = 0;
 
-    for (;;) {
+    while (!feof(res->in_file)) {
         /* Read the sectors from the input file to the buffer */
-        const size_t in_read =
-            fread(res->in_buffer, 1U, opts->buffer_size, res->in_file);
-
-        if ((in_read % opts->sector_size) != 0) {
-            print_error(
-                "data read from input file is not multiple of sector size");
-            return 1;
-        } else if (ferror(res->in_file)) {
-            print_error("cannot read from input file: %s", strerror(errno));
+        if (read_sector(res->in_file, res->in_buffer, opts->buffer_size,
+                        opts->sector_size) != 0) {
             return 1;
         }
 
         /* Read sectors from the reference file to the buffer */
-        const size_t ref_read =
-            fread(res->ref_buffer, 1U, opts->buffer_size, res->ref_file);
-
-        if ((ref_read % opts->sector_size) != 0) {
-            print_error(
-                "data read from reference file is not multiple of sector size");
-            return 1;
-        } else if (ferror(res->ref_file)) {
-            print_error("%s", strerror(errno));
-            return 1;
-        }
-
-        if (in_read != ref_read) {
-            print_error(
-                "data read from input file and reference file differ in size");
+        if (read_sector(res->ref_file, res->ref_buffer, opts->buffer_size,
+                        opts->sector_size) != 0) {
             return 1;
         }
 
         /* Process sectors in the buffer */
-        for (size_t i = 0; i < in_read; i += opts->sector_size) {
-            /* Compare the sectors  */
-            int changed = 0;
-            const size_t j_end = i + opts->sector_size;
-            for (size_t j = i; j < j_end; ++j) {
-                if (res->in_buffer[j] != res->ref_buffer[j]) {
-                    changed = 1;
-                    break;
-                }
-            }
-
-            if (changed) {
-                /* Write changed sector */
+        for (size_t buffer_offset = 0; buffer_offset < opts->buffer_size;
+             buffer_offset += opts->sector_size) {
+            if (memcmp(res->in_buffer + buffer_offset,
+                       res->ref_buffer + buffer_offset,
+                       opts->sector_size) != 0) {
+                /* Backup the changed sector */
                 if (out_buffer_index >= out_buffer_size) {
-                    /* The output buffer is full. Write it to the
-                     * output file.
+                    /* The output buffer is full. Write it to the output file.
                      */
-                    const size_t x = fwrite(res->out_buffer, 1U,
-                                            out_buffer_index, res->out_file);
-
-                    if (x != out_buffer_index) {
-                        print_error("cannot write to output file: %s",
-                                    strerror(errno));
+                    if (write_out_buffer(res->out_buffer, out_buffer_index,
+                                         res->out_file) != 1) {
                         return 1;
                     }
-
                     out_buffer_index = 0;
                 }
-
-                const uint64_t o = htole64(offset);
-
+                /* Write the next backup record */
+                const uint64_t o = htole64(input_file_offset);
                 memcpy(res->out_buffer + out_buffer_index, (void *)&o,
                        sizeof(o));
                 out_buffer_index += sizeof(o);
-                memcpy(res->out_buffer + out_buffer_index, &(res->in_buffer[i]),
-                       opts->sector_size);
+
+                memcpy(res->out_buffer + out_buffer_index,
+                       res->in_buffer + buffer_offset, opts->sector_size);
                 out_buffer_index += opts->sector_size;
             }
 
-            offset += opts->sector_size;
-        }
-
-        if (feof(res->in_file)) {
-            break;
+            input_file_offset += opts->sector_size;
         }
     }
 
     /* Write out the output buffer */
     if (out_buffer_index >= 0) {
-        const size_t x =
-            fwrite(res->out_buffer, 1U, out_buffer_index, res->out_file);
-
-        if (x != out_buffer_index) {
-            print_error("cannot write to output file: %s", strerror(errno));
+        if (write_out_buffer(res->out_buffer, out_buffer_index,
+                             res->out_file) != 1) {
             return 1;
         }
     }
