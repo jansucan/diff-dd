c87b50692508c6556bf9e66b746236a06a192e62
Jan Sucan <jan@jansucan.com>
2024-12-12 17:20:56 +0100

    makefile: Rename variables from C to CXX for C++

--- config.mk
+++ config.mk
@@ -4,5 +4,5 @@ PROGRAM_VERSION = 3.0.0-prealpha
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-CC=g++
-CFLAGS=-Wall -Wextra -Werror -std=c++17
+CXX=g++
+CXXFLAGS=-Wall -Wextra -Werror -std=c++17
